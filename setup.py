412dc167cf71b47081a470de49208b4c8443038b
erickpeirson <brp53@cornell.edu>
2019-04-11 08:22:09 -0400

    minor fix to setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-vault',
-    version='0.0.2',
-    packages=['arxiv.vault'],
+    version='0.0.3',
+    packages=[f'arxiv.{package}' for package in find_packages('arxiv')],
     zip_safe=False,
     install_requires=[
         'hvac==0.8.2'
