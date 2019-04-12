b6af2f4f9acaa5a3fbc1eaf0e1b96089e20dc08f
erickpeirson <brp53@cornell.edu>
2019-04-12 13:44:43 -0400

    ARXIVNG-2055 fix for unitialized adapter

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-vault',
-    version='0.0.6',
+    version='0.0.7',
     packages=[f'arxiv.{package}' for package in find_packages('arxiv')],
     zip_safe=False,
     install_requires=[
