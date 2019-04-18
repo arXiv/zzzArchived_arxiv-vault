369ea1ed5f1cc8a1fcd49946f89d2ac14d32c422
erickpeirson <brp53@cornell.edu>
2019-04-18 10:33:58 -0400

    incremented patch version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-vault',
-    version='0.0.13',
+    version='0.0.14',
     packages=[f'arxiv.{package}' for package in find_packages('arxiv')],
     zip_safe=False,
     install_requires=[
